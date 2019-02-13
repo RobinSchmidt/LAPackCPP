cc3cda87c8f918ddf4a41aa4fc9de31125aa72f4
Robin Schmidt <robin@rs-met.com>
2019-02-13 12:53:46 +0100

    added trsm

--- Source/LaPackCPP/Blas.cpp
+++ Source/LaPackCPP/Blas.cpp
@@ -872,6 +872,351 @@ int gemm(char *transa, char *transb, integer *m, integer *n, integer *k, T *alph
 
 } // gemm
 
+//-------------------------------------------------------------------------------------------------
+
+// translated from dtrsm, Reference BLAS level3 routine (version 3.7.0) */
+template<class T>
+int trsm(char *side, char *uplo, char *transa, char *diag, integer *m, integer *n, T *alpha, 
+  T *a, integer * lda, T *b, integer *ldb, ftnlen side_len, ftnlen uplo_len, ftnlen transa_len, 
+  ftnlen diag_len)
+{
+  // System generated locals 
+  integer a_dim1, a_offset, b_dim1, b_offset, i__1, i__2, i__3;
+
+  // Local variables
+  static integer i__, j, k, info;
+  static T temp;
+  static logical lside;
+  extern logical lsame_(char *, char *, ftnlen, ftnlen);
+  static integer nrowa;
+  static logical upper;
+  extern int xerbla_(char *, integer *, ftnlen);
+  static logical nounit;
+
+  // Parameter adjustments
+  a_dim1 = *lda;
+  a_offset = 1 + a_dim1;
+  a -= a_offset;
+  b_dim1 = *ldb;
+  b_offset = 1 + b_dim1;
+  b -= b_offset;
+
+  // Function Body 
+  lside = lsame_(side, "L", (ftnlen)1, (ftnlen)1);
+  if (lside) {
+    nrowa = *m;
+  } else {
+    nrowa = *n;
+  }
+  nounit = lsame_(diag, "N", (ftnlen)1, (ftnlen)1);
+  upper = lsame_(uplo, "U", (ftnlen)1, (ftnlen)1);
+
+  info = 0;
+  if (! lside && ! lsame_(side, "R", (ftnlen)1, (ftnlen)1)) {
+    info = 1;
+  } else if (! upper && ! lsame_(uplo, "L", (ftnlen)1, (ftnlen)1)) {
+    info = 2;
+  } else if (! lsame_(transa, "N", (ftnlen)1, (ftnlen)1) && ! lsame_(transa,
+    "T", (ftnlen)1, (ftnlen)1) && ! lsame_(transa, "C", (ftnlen)1, (
+      ftnlen)1)) {
+    info = 3;
+  } else if (! lsame_(diag, "U", (ftnlen)1, (ftnlen)1) && ! lsame_(diag, 
+    "N", (ftnlen)1, (ftnlen)1)) {
+    info = 4;
+  } else if (*m < 0) {
+    info = 5;
+  } else if (*n < 0) {
+    info = 6;
+  } else if (*lda < max(1,nrowa)) {
+    info = 9;
+  } else if (*ldb < max(1,*m)) {
+    info = 11;
+  }
+  if (info != 0) {
+    xerbla_("DTRSM ", &info, (ftnlen)6);
+    return 0;
+  }
+
+  // Quick return if possible
+
+  if (*m == 0 || *n == 0) {
+    return 0;
+  }
+
+  // And when  alpha.eq.zero.
+
+  if (*alpha == 0.) {
+    i__1 = *n;
+    for (j = 1; j <= i__1; ++j) {
+      i__2 = *m;
+      for (i__ = 1; i__ <= i__2; ++i__) {
+        b[i__ + j * b_dim1] = 0.;
+        // L10: 
+      }
+      // L20: 
+    }
+    return 0;
+  }
+
+  // Start the operations.
+
+  if (lside) {
+    if (lsame_(transa, "N", (ftnlen)1, (ftnlen)1)) {
+
+      // Form  B := alpha*inv( A )*B.
+
+      if (upper) {
+        i__1 = *n;
+        for (j = 1; j <= i__1; ++j) {
+          if (*alpha != 1.) {
+            i__2 = *m;
+            for (i__ = 1; i__ <= i__2; ++i__) {
+              b[i__ + j * b_dim1] = *alpha * b[i__ + j * b_dim1]
+                ;
+              // L30:
+            }
+          }
+          for (k = *m; k >= 1; --k) {
+            if (b[k + j * b_dim1] != 0.) {
+              if (nounit) {
+                b[k + j * b_dim1] /= a[k + k * a_dim1];
+              }
+              i__2 = k - 1;
+              for (i__ = 1; i__ <= i__2; ++i__) {
+                b[i__ + j * b_dim1] -= b[k + j * b_dim1] * a[
+                  i__ + k * a_dim1];
+                // L40: 
+              }
+            }
+            // L50:
+          }
+          // L60:
+        }
+      } else {
+        i__1 = *n;
+        for (j = 1; j <= i__1; ++j) {
+          if (*alpha != 1.) {
+            i__2 = *m;
+            for (i__ = 1; i__ <= i__2; ++i__) {
+              b[i__ + j * b_dim1] = *alpha * b[i__ + j * b_dim1]
+                ;
+              // L70: 
+            }
+          }
+          i__2 = *m;
+          for (k = 1; k <= i__2; ++k) {
+            if (b[k + j * b_dim1] != 0.) {
+              if (nounit) {
+                b[k + j * b_dim1] /= a[k + k * a_dim1];
+              }
+              i__3 = *m;
+              for (i__ = k + 1; i__ <= i__3; ++i__) {
+                b[i__ + j * b_dim1] -= b[k + j * b_dim1] * a[
+                  i__ + k * a_dim1];
+                // L80: 
+              }
+            }
+            // L90: 
+          }
+          // L100: 
+        }
+      }
+    } else {
+
+      // Form  B := alpha*inv( A**T )*B.
+
+      if (upper) {
+        i__1 = *n;
+        for (j = 1; j <= i__1; ++j) {
+          i__2 = *m;
+          for (i__ = 1; i__ <= i__2; ++i__) {
+            temp = *alpha * b[i__ + j * b_dim1];
+            i__3 = i__ - 1;
+            for (k = 1; k <= i__3; ++k) {
+              temp -= a[k + i__ * a_dim1] * b[k + j * b_dim1];
+              // L110: 
+            }
+            if (nounit) {
+              temp /= a[i__ + i__ * a_dim1];
+            }
+            b[i__ + j * b_dim1] = temp;
+            // L120: 
+          }
+          // L130: 
+        }
+      } else {
+        i__1 = *n;
+        for (j = 1; j <= i__1; ++j) {
+          for (i__ = *m; i__ >= 1; --i__) {
+            temp = *alpha * b[i__ + j * b_dim1];
+            i__2 = *m;
+            for (k = i__ + 1; k <= i__2; ++k) {
+              temp -= a[k + i__ * a_dim1] * b[k + j * b_dim1];
+              // L140: 
+            }
+            if (nounit) {
+              temp /= a[i__ + i__ * a_dim1];
+            }
+            b[i__ + j * b_dim1] = temp;
+            // L150: 
+          }
+          // L160: 
+        }
+      }
+    }
+  } else {
+    if (lsame_(transa, "N", (ftnlen)1, (ftnlen)1)) {
+
+      // Form  B := alpha*B*inv( A ). 
+
+      if (upper) {
+        i__1 = *n;
+        for (j = 1; j <= i__1; ++j) {
+          if (*alpha != 1.) {
+            i__2 = *m;
+            for (i__ = 1; i__ <= i__2; ++i__) {
+              b[i__ + j * b_dim1] = *alpha * b[i__ + j * b_dim1]
+                ;
+              // L170: 
+            }
+          }
+          i__2 = j - 1;
+          for (k = 1; k <= i__2; ++k) {
+            if (a[k + j * a_dim1] != 0.) {
+              i__3 = *m;
+              for (i__ = 1; i__ <= i__3; ++i__) {
+                b[i__ + j * b_dim1] -= a[k + j * a_dim1] * b[
+                  i__ + k * b_dim1];
+                // L180: 
+              }
+            }
+            // L190: 
+          }
+          if (nounit) {
+            temp = 1. / a[j + j * a_dim1];
+            i__2 = *m;
+            for (i__ = 1; i__ <= i__2; ++i__) {
+              b[i__ + j * b_dim1] = temp * b[i__ + j * b_dim1];
+              // L200: 
+            }
+          }
+          // L210: 
+        }
+      } else {
+        for (j = *n; j >= 1; --j) {
+          if (*alpha != 1.) {
+            i__1 = *m;
+            for (i__ = 1; i__ <= i__1; ++i__) {
+              b[i__ + j * b_dim1] = *alpha * b[i__ + j * b_dim1]
+                ;
+              // L220: 
+            }
+          }
+          i__1 = *n;
+          for (k = j + 1; k <= i__1; ++k) {
+            if (a[k + j * a_dim1] != 0.) {
+              i__2 = *m;
+              for (i__ = 1; i__ <= i__2; ++i__) {
+                b[i__ + j * b_dim1] -= a[k + j * a_dim1] * b[
+                  i__ + k * b_dim1];
+                // L230: 
+              }
+            }
+            // L240: 
+          }
+          if (nounit) {
+            temp = 1. / a[j + j * a_dim1];
+            i__1 = *m;
+            for (i__ = 1; i__ <= i__1; ++i__) {
+              b[i__ + j * b_dim1] = temp * b[i__ + j * b_dim1];
+              // L250: 
+            }
+          }
+          // L260:
+        }
+      }
+    } else {
+
+      // Form  B := alpha*B*inv( A**T ).
+
+      if (upper) {
+        for (k = *n; k >= 1; --k) {
+          if (nounit) {
+            temp = 1. / a[k + k * a_dim1];
+            i__1 = *m;
+            for (i__ = 1; i__ <= i__1; ++i__) {
+              b[i__ + k * b_dim1] = temp * b[i__ + k * b_dim1];
+              // L270: 
+            }
+          }
+          i__1 = k - 1;
+          for (j = 1; j <= i__1; ++j) {
+            if (a[j + k * a_dim1] != 0.) {
+              temp = a[j + k * a_dim1];
+              i__2 = *m;
+              for (i__ = 1; i__ <= i__2; ++i__) {
+                b[i__ + j * b_dim1] -= temp * b[i__ + k * 
+                  b_dim1];
+                // L280: 
+              }
+            }
+            // L290: 
+          }
+          if (*alpha != 1.) {
+            i__1 = *m;
+            for (i__ = 1; i__ <= i__1; ++i__) {
+              b[i__ + k * b_dim1] = *alpha * b[i__ + k * b_dim1]
+                ;
+              // L300: 
+            }
+          }
+          // L310: 
+        }
+      } else {
+        i__1 = *n;
+        for (k = 1; k <= i__1; ++k) {
+          if (nounit) {
+            temp = 1. / a[k + k * a_dim1];
+            i__2 = *m;
+            for (i__ = 1; i__ <= i__2; ++i__) {
+              b[i__ + k * b_dim1] = temp * b[i__ + k * b_dim1];
+              // L320:
+            }
+          }
+          i__2 = *n;
+          for (j = k + 1; j <= i__2; ++j) {
+            if (a[j + k * a_dim1] != 0.) {
+              temp = a[j + k * a_dim1];
+              i__3 = *m;
+              for (i__ = 1; i__ <= i__3; ++i__) {
+                b[i__ + j * b_dim1] -= temp * b[i__ + k * 
+                  b_dim1];
+                // L330: 
+              }
+            }
+            // L340: 
+          }
+          if (*alpha != 1.) {
+            i__2 = *m;
+            for (i__ = 1; i__ <= i__2; ++i__) {
+              b[i__ + k * b_dim1] = *alpha * b[i__ + k * b_dim1]
+                ;
+              // L350: 
+            }
+          }
+          // L360: 
+        }
+      }
+    }
+  }
+
+  return 0;
+
+} // trsm 
+
+
+
+
 //=================================================================================================
 
 // explicit template instantiations - todo: move them into separate files, one file per
