9f033c701a7d8671f4d29252eb353cb7612745d3
Robin Schmidt <robin@rs-met.com>
2019-02-13 13:49:28 +0100

    added gemv code

--- Source/LaPackCPP/Blas.cpp
+++ Source/LaPackCPP/Blas.cpp
@@ -639,6 +639,197 @@ int gbmv(char *trans, integer *m, integer *n, integer *kl, integer *ku, T *alpha
 
 } // gbmv
 
+//-------------------------------------------------------------------------------------------------
+
+// translated from dgemv, Reference BLAS level2 routine (version 3.7.0)
+template<class T>
+int gemv(char *trans, integer *m, integer *n, T *alpha, T *a, integer *lda, T *x, integer *incx, 
+  T *beta, T *y, integer *incy, ftnlen trans_len)
+{
+  /* System generated locals */
+  integer a_dim1, a_offset, i__1, i__2;
+
+  /* Local variables */
+  static integer i__, j, ix, iy, jx, jy, kx, ky, info;
+  static T temp;
+  static integer lenx, leny;
+  extern logical lsame_(char *, char *, ftnlen, ftnlen);
+  extern /* Subroutine */ int xerbla_(char *, integer *, ftnlen);
+
+  /*     Test the input parameters. */
+
+  /* Parameter adjustments */
+  a_dim1 = *lda;
+  a_offset = 1 + a_dim1;
+  a -= a_offset;
+  --x;
+  --y;
+
+  /* Function Body */
+  info = 0;
+  if (! lsame_(trans, "N", (ftnlen)1, (ftnlen)1) && ! lsame_(trans, "T", (
+    ftnlen)1, (ftnlen)1) && ! lsame_(trans, "C", (ftnlen)1, (ftnlen)1)
+    ) {
+    info = 1;
+  } else if (*m < 0) {
+    info = 2;
+  } else if (*n < 0) {
+    info = 3;
+  } else if (*lda < max(1,*m)) {
+    info = 6;
+  } else if (*incx == 0) {
+    info = 8;
+  } else if (*incy == 0) {
+    info = 11;
+  }
+  if (info != 0) {
+    xerbla_("DGEMV ", &info, (ftnlen)6);
+    return 0;
+  }
+
+  /*     Quick return if possible. */
+
+  if (*m == 0 || *n == 0 || *alpha == 0. && *beta == 1.) {
+    return 0;
+  }
+
+  /*     Set  LENX  and  LENY, the lengths of the vectors x and y, and set */
+  /*     up the start points in  X  and  Y. */
+
+  if (lsame_(trans, "N", (ftnlen)1, (ftnlen)1)) {
+    lenx = *n;
+    leny = *m;
+  } else {
+    lenx = *m;
+    leny = *n;
+  }
+  if (*incx > 0) {
+    kx = 1;
+  } else {
+    kx = 1 - (lenx - 1) * *incx;
+  }
+  if (*incy > 0) {
+    ky = 1;
+  } else {
+    ky = 1 - (leny - 1) * *incy;
+  }
+
+  /*     Start the operations. In this version the elements of A are */
+  /*     accessed sequentially with one pass through A. */
+
+  /*     First form  y := beta*y. */
+
+  if (*beta != 1.) {
+    if (*incy == 1) {
+      if (*beta == 0.) {
+        i__1 = leny;
+        for (i__ = 1; i__ <= i__1; ++i__) {
+          y[i__] = 0.;
+          /* L10: */
+        }
+      } else {
+        i__1 = leny;
+        for (i__ = 1; i__ <= i__1; ++i__) {
+          y[i__] = *beta * y[i__];
+          /* L20: */
+        }
+      }
+    } else {
+      iy = ky;
+      if (*beta == 0.) {
+        i__1 = leny;
+        for (i__ = 1; i__ <= i__1; ++i__) {
+          y[iy] = 0.;
+          iy += *incy;
+          /* L30: */
+        }
+      } else {
+        i__1 = leny;
+        for (i__ = 1; i__ <= i__1; ++i__) {
+          y[iy] = *beta * y[iy];
+          iy += *incy;
+          /* L40: */
+        }
+      }
+    }
+  }
+  if (*alpha == 0.) {
+    return 0;
+  }
+  if (lsame_(trans, "N", (ftnlen)1, (ftnlen)1)) {
+
+    /*        Form  y := alpha*A*x + y. */
+
+    jx = kx;
+    if (*incy == 1) {
+      i__1 = *n;
+      for (j = 1; j <= i__1; ++j) {
+        temp = *alpha * x[jx];
+        i__2 = *m;
+        for (i__ = 1; i__ <= i__2; ++i__) {
+          y[i__] += temp * a[i__ + j * a_dim1];
+          /* L50: */
+        }
+        jx += *incx;
+        /* L60: */
+      }
+    } else {
+      i__1 = *n;
+      for (j = 1; j <= i__1; ++j) {
+        temp = *alpha * x[jx];
+        iy = ky;
+        i__2 = *m;
+        for (i__ = 1; i__ <= i__2; ++i__) {
+          y[iy] += temp * a[i__ + j * a_dim1];
+          iy += *incy;
+          /* L70: */
+        }
+        jx += *incx;
+        /* L80: */
+      }
+    }
+  } else {
+
+    /*        Form  y := alpha*A**T*x + y. */
+
+    jy = ky;
+    if (*incx == 1) {
+      i__1 = *n;
+      for (j = 1; j <= i__1; ++j) {
+        temp = 0.;
+        i__2 = *m;
+        for (i__ = 1; i__ <= i__2; ++i__) {
+          temp += a[i__ + j * a_dim1] * x[i__];
+          /* L90: */
+        }
+        y[jy] += *alpha * temp;
+        jy += *incy;
+        /* L100: */
+      }
+    } else {
+      i__1 = *n;
+      for (j = 1; j <= i__1; ++j) {
+        temp = 0.;
+        ix = kx;
+        i__2 = *m;
+        for (i__ = 1; i__ <= i__2; ++i__) {
+          temp += a[i__ + j * a_dim1] * x[ix];
+          ix += *incx;
+          /* L110: */
+        }
+        y[jy] += *alpha * temp;
+        jy += *incy;
+        /* L120: */
+      }
+    }
+  }
+
+  return 0;
+
+  /*     End of DGEMV . */
+
+} /* dgemv_ */
+
 //=================================================================================================
 // BLAS level 3 routines
 
